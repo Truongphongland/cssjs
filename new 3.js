4ffa90c20fa66f047f06985142c18a8686e7664e
Truongphongland <105635615+Truongphongland@users.noreply.github.com>
2022-05-17 11:15:45 +0700

    Add files via upload

---
+++ new 3.js
@@ -0,0 +1,17 @@
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/%C3%9D-Th%E1%BB%A9c-%C4%90%E1%BA%A9y-L%C3%B9i-D%E1%BB%8Bch-B%E1%BB%87nh-108397031475842'>https://www.facebook.com/%C3%9D-Th%E1%BB%A9c-%C4%90%E1%BA%A9y-L%C3%B9i-D%E1%BB%8Bch-B%E1%BB%87nh-108397031475842</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
