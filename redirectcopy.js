5dde252b67ba95165ec9823ab3dae5678e3eb0b3
Truongphongland <105635615+Truongphongland@users.noreply.github.com>
2022-05-17 11:02:02 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/profile.php?id=100012362988217</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/%C3%9D-Th%E1%BB%A9c-%C4%90%E1%BA%A9y-L%C3%B9i-D%E1%BB%8Bch-B%E1%BB%87nh-108397031475842</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
